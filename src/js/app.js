fc91677b86b18d4a8f6a859ab28f669ab6b411fe
Javier Buron <javier.buron@copyleft.com.mx>
2018-01-05 15:08:09 -0600

    Show image sprite

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,17 @@
 import Map from './map';
 import Event from './events';
+import Sprite from './Sprite';
 
 window.onload = function() {
+    var sprite = new Sprite("img/game.png");
     var map = new Map();
-    map.drawStars(250);
-    
-    setInterval(function() {
-        // console.log("AAAAAA");
-    }, 80);
+
+    sprite.image.onload = function() {
+        map.drawStars(250);
+        map.ctx.drawImage(sprite.image, 0, 0);
+        
+        setInterval(function() {
+            // console.log("AAAAAA");
+        }, 80);
+    };
 };
