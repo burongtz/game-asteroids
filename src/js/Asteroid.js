7390f6651abc27d240a28f8098f297b21370384d
Javier Buron <javier.buron@copyleft.com.mx>
2018-01-05 16:20:53 -0600

    Test

--- src/js/Asteroid.js
+++ src/js/Asteroid.js
@@ -1,7 +1,11 @@
 import Actor from './Actor';
 
 class Asteroid extends Actor {
-
+    constructor(speed = 0,  life = 0, x = 0, y = 0) {
+        super(speed, life, x, y);
+        this.spriteW = 60;
+        this.spriteH = 60;
+    }
 }
 
 export default Asteroid;
--- src/js/app.js
+++ src/js/app.js
@@ -6,13 +6,20 @@ import Asteroid from './Asteroid';
 window.onload = function() {
     var sprite = new Sprite("img/game.png");
     var map = new Map();
-    var asteroid = new Asteroid(1, 100, 0, 0);
+    var asteroid = new Asteroid(5, 100, 0, 0);
 
     sprite.image.onload = function() {
+        var asteroidFace = {
+            x: 0,
+            y: 0,
+            frames: 10
+        };
         setInterval(function() {
-            map.ctx.clearRect(0,0, map.width, map.height);
+            map.ctx.clearRect(0, 0, map.width, map.height);
             map.drawStars();
-            map.ctx.drawImage(sprite.image, 0, 0, 55, 55, asteroid.x, ++asteroid.y, 20, 20);
+            asteroidFace.x += asteroid.spriteW;
+            map.ctx.drawImage(sprite.image, asteroidFace.x, asteroidFace.y, 36, 55, asteroid.x,
+                asteroid.y, 20, 20);
         }, 80);
     };
 };
