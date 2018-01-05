2a4ce5b2d6f16bd97cdf420456553a80c927ef9d
Javier Buron <javier.buron@copyleft.com.mx>
2018-01-05 17:27:58 -0600

    Update asteroid method

--- src/js/app.js
+++ src/js/app.js
@@ -2,29 +2,33 @@ import Map from './map';
 import Sprite from './Sprite';
 import Ship from './Ship';
 import Asteroid from './Asteroid';
+import Helper from './helper';
 
 require('./events');
 
 window.onload = function() {
     var sprite = new Sprite("img/game.png");
-    var asteroid = new Asteroid(1, 100, 0, 0);
-
-    window.map = new Map();
+    
+    window.asteroids = [];
+    window.map = new Map(sprite.image);
     window.ship = new Ship(map, sprite);
-    sprite.image.onload = function() {
-        var asteroidFace = {
-            x: 0,
-            y: 0,
-            frames: 10
-        };
+    
+    // Create asteroids
+    for (var i = 0, size = Helper.getRandom(1, Helper.ASTEROID_LIMIT); i < size; i++) {
+        window.asteroids.push(
+            new Asteroid(Helper.getRandom(1, 3), 100, Helper.getRandom(0 , map.width - 20), -15)
+        );
+    }
 
+    sprite.image.onload = function() {
         setInterval(function() {
-            map.ctx.clearRect(0, 0, map.width, map.height);
-            map.drawStars();
-            asteroidFace.x += asteroid.spriteW;
-            map.ctx.drawImage(sprite.image, asteroidFace.x, asteroidFace.y, asteroid.spriteW, asteroid.spriteH, asteroid.x,
-                asteroid.y, 20, 20);
+            // Change asteroids position
+            window.asteroids.forEach(function(asteroid) {
+                asteroid.y += asteroid.speed;
+            });
+
+            map.drawAll(window.asteroids);
             ship.move();
-        }, 80);
+        }, 40);
     };
 };
