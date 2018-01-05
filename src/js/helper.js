d92236023c4e28bcd867d3ab94839b2d3182965f
Javier Buron <javier.buron@copyleft.com.mx>
2018-01-05 17:26:44 -0600

    Create static var ASTEROID_LIMIT

--- src/js/helper.js
+++ src/js/helper.js
@@ -1,4 +1,7 @@
 class Helper {
+    static get ASTEROID_LIMIT() {
+        return 10;
+    };
     static getRandom(min, max) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
